2ddb1899ef55150e826fd2676f3de97f87b20f80
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-03-29 17:53:50 +0300

    finished app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import routes from './routes';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h3>App component</h3>
-    </div>
+    <Router>
+      <main className='container row'>
+        <section className='col s4 card-panel pink white-text'>
+          <h6>side nav</h6>
+          <hr />
+          <ul>
+            <li>
+              <Link to='/'>home</Link>
+            </li>
+            <li>
+              <Link to='/about'>about</Link>
+            </li>
+            <li>
+              <Link to='/info'>info</Link>
+            </li>
+            <li>
+              <Link to='/more'>more</Link>
+            </li>
+          </ul>
+          <section>
+            {routes.map(route => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={route.path}
+                component={route.sideBar}
+              />
+            ))}
+          </section>
+        </section>
+        <section className='main col s8'>
+          <h3>main contents</h3>
+          <hr />
+          <Switch>
+            {routes.map(route => (
+              <Route
+                exact={route.exact}
+                key={route.path}
+                path={route.path}
+                component={route.main}
+              />
+            ))}
+          </Switch>
+        </section>
+      </main>
+    </Router>
   );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import Home from '../components/Home';
+import About from '../components/About';
+import More from '../components/More';
+import Info from '../components/Info';
 
 export default [
   {
     path: '/',
+    exact: true,
     sideBar: () => <h5>home</h5>,
     main: Home
+  },
+  {
+    path: '/about',
+    sideBar: () => <h5>about</h5>,
+    main: About
+  },
+  {
+    path: '/info',
+    sideBar: () => <h5>info</h5>,
+    main: Info
+  },
+  {
+    path: '/more',
+    sideBar: () => <h5>more</h5>,
+    main: More
   }
 ];
